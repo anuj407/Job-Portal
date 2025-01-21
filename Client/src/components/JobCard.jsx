fc04eef9a5a65223fd1f530763e6700cecb555a2
anuj407 <anuj97953a@gmail.com>
2025-01-21 21:56:46 +0530

    Created JobCard Component

---
+++ Client/src/components/JobCard.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import {assets} from "../assets/assets"
+function JobCard({job}) {
+  return (
+    <div className="w-[1/3] h-[19rem] px-5 py-4 border-2 border-zinc-300 rounded-md flex flex-col justify-evenly">
+        <img className="w-8" src={assets.company_icon} alt="" />
+        <h1 className="text-xl font-semibold ">{job.title}</h1>  
+        <div className=" flex gap-3"> 
+          <h4 className="bg-blue-100 px-3 py-1 text-sm rounded-md border-[1px] border-blue-300">{job.location}</h4>
+          <h4 className="px-3 py-1 text-sm rounded-md bg-red-50 border-[1px] border-red-200">{job.level}</h4>
+        </div>   
+        <p className="text-zinc-700 text-sm " dangerouslySetInnerHTML={{__html :job.description.slice(0,150)}}></p>
+        <div className=" flex gap-3">
+            <button className="bg-blue-600 text-white px-3 py-1 rounded-md">Apply Now</button>
+            <button className="text-gray-600 px-3 py-1 border-2 rounded-md">View Details</button>
+        </div>
+    </div>
+  )
+}
+
+export default JobCard
