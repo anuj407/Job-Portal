6922dc02015201d85d2e75bb6f1912c75093990e
anuj407 <anuj97953a@gmail.com>
2025-01-25 18:31:31 +0530

    Responsive

--- Client/src/components/AppDownload.jsx
+++ Client/src/components/AppDownload.jsx
@@ -2,20 +2,20 @@ import { assets } from "../assets/assets"
 
 function AppDownload() {
   return (
-    <div className=" h-[45rem] flex justify-center items-center">
+    <div className="h-[30rem] sm:h-[45rem] flex justify-center items-center">
         <div className="w-4/5 h-2/3 bg-[#F8F4FF] flex justify-center rounded-md">
-            <div className="w-1/2 flex flex-col justify-center gap-6">
-                <h1 className="text-3xl font-bold">Download The Mobile App For Better Exprience</h1>
-                <div className=" flex gap-4">
-                    <a href="">
-                        <img src={assets.play_store} alt="" />
+            <div className=" sm:w-1/2 flex flex-col justify-center gap-6">
+                <h1 className="max-sm:text-center text-3xl font-bold">Download The Mobile App For Better Exprience</h1>
+                <div className=" flex gap-4 max-sm:px-3">
+                    <a className="" href="">
+                        <img className="w-full" src={assets.play_store} alt="" />
                     </a>
-                    <a href="">
-                        <img src={assets.app_store} alt="" />
+                    <a className="" href="">
+                        <img className="w-full" src={assets.app_store} alt="" />
                     </a>
                 </div>
             </div>
-            <img className="w-1/4 h-3/4 object-cover self-end" src={assets.app_main_img} alt="" />
+            <img className="max-sm:hidden w-1/2 lg:w-1/4 h-3/4 object-fill self-end" src={assets.app_main_img} alt="" />
         </div>
     </div>
   )
--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -3,14 +3,14 @@ import { assets } from "../assets/assets"
 function Footer() {
   return (
    
-        <div className="max-w-6xl mx-auto flex justify-between pb-4">
-            <div className="w-2/3 flex items-center gap-3">
+        <div className="max-w-6xl mx-auto flex justify-evenly sm:justify-between pb-4">
+            <div className="sm:w-2/3 flex items-center gap-3">
                 <img src={assets.company_icon} alt="" />
                 <h2 className="text-xl font-semibold">Insider<span className="text-lg font-normal">jobs</span></h2>
-                <span className="text-xl">|</span>
-                <p>Copyright @GreatStack.dev | All right reserved.</p>
+                <span className="max-sm:hidden text-xl">|</span>
+                <p className="max-sm:hidden">Copyright @GreatStack.dev | All right reserved.</p>
             </div>
-            <div className="flex">
+            <div className="flex gap-1">
                 <a href="">
                     <img src={assets.facebook_icon} alt="" />
                 </a>
