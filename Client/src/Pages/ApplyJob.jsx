e00b358e6b27735e900c3dcb12fa1be861dc01b2
anuj407 <anuj97953a@gmail.com>
2025-01-30 22:45:02 +0530

    build apply-job page

--- Client/src/Pages/ApplyJob.jsx
+++ Client/src/Pages/ApplyJob.jsx
@@ -1,40 +1,40 @@
-/* eslint-disable react-hooks/exhaustive-deps */
+
 import { useContext, useEffect, useState } from "react"
 import {useParams} from "react-router-dom"
 import { AppContext } from "../context/AppContext"
 import Navbar from "../components/Navbar"
-import { PiBagBold } from "react-icons/pi";
-import { IoLocationOutline } from "react-icons/io5";
-import { IoIosContact } from "react-icons/io";
-import { GiMoneyStack } from "react-icons/gi";
-
+import { assets } from "../assets/assets";
+import kConvert from "k-convert";
 function ApplyJob() {
   const {id} = useParams()
   const [JobData, setJobData]= useState(null)
   const {jobs} = useContext(AppContext)
   const fetchJobData = async ()=>{
-     const data =jobs.filter(job => job._id === id)
+     const data = jobs.filter(job => job._id === id)
      setJobData(data[0])
   }
-  console.log(JobData)
   useEffect(()=>{
     fetchJobData()
-  },[id])
+  })
   return (
     <div>
         <Navbar/>
-        <div className="max-w-screen-xl mx-auto mt-10 h-[18rem] border-2 border-[#9CDEFC] bg-[#F0F9FF] rounded-md">
-          <div className="">
-            <img src="" alt="" />
+        <div className="max-w-screen-xl mx-auto mt-10 h-[18rem] border-2 border-[#9CDEFC] bg-[#F0F9FF] rounded-md flex items-center justify-center gap-x-3 pl-4">
+          <div className="w-[6rem] h-[6rem] rounded-md border flex justify-center items-center bg-white">
+            <img className="w-[4rem]" src={JobData?.companyId.image} alt="" />
           </div>
-          <div className="">
-            <h1></h1>
-            <div className="">
-              <span><PiBagBold/></span>
-              <span><IoLocationOutline/></span>
-              <span><IoIosContact/></span>
-              <span><GiMoneyStack/></span>
+          <div className="w-2/3 h-[6rem] flex flex-col justify-evenly">
+            <h1 className="text-[2rem] leading-none font-bold">{JobData?.title}</h1>
+            <div className="flex gap-3">
+              <span className="flex items-center gap-1"><img className="w-4" src={assets.suitcase_icon} alt="" /> {JobData?.companyId.name} </span>
+              <span className="flex items-center gap-1"><img src={assets.location_icon} alt="" />{JobData?.location}</span>
+              <span className="flex items-center gap-1"><img src={assets.person_icon} alt="" />{JobData?.companyId.email}</span>
+              <span className="flex items-center gap-1"><img src={assets.money_icon} alt="" /> CTC : {kConvert.convertTo(JobData?.salary)}</span>
             </div>
+          </div>
+          <div className="w-1/6">
+            <button className="px-10 py-2 bg-blue-600 text-white rounded-xl">Apply Job</button>
+            <p>Posted 3 months ago</p>
           </div>
         </div>
     </div>
